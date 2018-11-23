e613711889bae5af8801f2b66e4bfb91fad185a9
DESKTOP-8R3FD9M\hooloop_PC03 <thomaskao@hooloop.com>
2018-11-23 10:41:57 +0800

    Init

---
+++ Assets/VideoPlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoPlayerController : MonoBehaviour {
+
+    VideoPlayer myVideoPlayer;
+
+    private int counter = 0;
+
+    public Text loopCounter;
+
+
+	// Use this for initialization
+	void Start () {
+        myVideoPlayer = GetComponent<VideoPlayer>();
+
+        myVideoPlayer.loopPointReached += OnVideoLoopPointReached;
+    }
+
+    public void OnVideoLoopPointReached(VideoPlayer vp)
+    {
+        myVideoPlayer.Stop();
+        counter++;
+        loopCounter.text = counter.ToString();
+        myVideoPlayer.Play();
+    }
+
+}
